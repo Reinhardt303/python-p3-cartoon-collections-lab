5448a8efaa458d35d34982e6600e84235fe43e59
Thomas <thomasrains01@gmail.com>
2025-01-18 22:00:07 -0800

    submits lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    i = 1
+    for name in names:
+        print(f'{i}. {name}')
+        i +=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    capital_calls = [f'{call.capitalize()}!'for call in calls]
+    return capital_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+        
 
-def find_the_cheese():
+
+def find_the_cheese(ingredients):
+    cheeses =  ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
     pass
